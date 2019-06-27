28dac2d947f919e0f51ba47c92bc9483fdc31ecd
Student <example@example.com>
2019-06-26 19:02:59 -0700

    add objects

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+var triangle1 = {
+  name: "equilateral",
+  sides: [num === num],
+}
+var triangle2 = {
+  name: "isosceles",
+  sides: 2
+}
+var triangle3 = {
+  name: "scalene",
+  sides: "no equal",
+}
+
+if(!obj)
